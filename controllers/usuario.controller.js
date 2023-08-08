3b2934850aa53c8ae53a04cb64e2f0741dfd539c
Ricardo Terán <39249155+Rikardo816@users.noreply.github.com>
2023-08-08 11:41:38 -0500

    add: new files about users
    se crearon los archivos para empezar a trabajar con los usuarios

---
+++ controllers/usuario.controller.js
@@ -0,0 +1,60 @@
+'use strict'
+const Usuario = require('../models/usuario');
+const userController = {
+
+    // Guardar usuario
+    saveUsuario: async function (req, res) {
+        try {
+            var usuario = new Usuario();
+            var params = req.body;
+            usuario.nombre = params.nombre;
+            usuario.apellido = params.apellido;
+            usuario.nacimiento = params.nacimiento;
+            usuario.correo = params.correo;
+            usuario.identificacion = params.identificacion;
+            usuario.telefono = params.telefono;
+            usuario.vueloReservados = params.vueloReservados;
+            usuario.metodosDePago = params.metodosDePago;
+
+            var usuarioStored = await usuario.save();
+
+            if (!usuarioStored) {
+                return res.status(404).send({ message: 'No se pudo guardar el usuario' });
+            }
+            return res.status(201).send({ usuario: usuarioStored });
+        } catch (err) {
+            return res.status(500).send({ message: 'Error al guardar los datos' });
+        }
+    },
+
+    //Obtener usuarios
+    getUsuarios: async function (req, res) {
+        try {
+            var usuarios = await Usuario.find().sort('_id').exec();
+            if (usuarios.length == 0) {
+                return res.status(404).send({ message: 'No se encontraron usuarios' });
+            }
+            return res.status(200).send({ usuarios });
+        } catch (err) {
+            return res.status(500).send({ message: 'Error al obtener los datos' });
+        }
+    },
+
+    // Obtener usuario
+    getUsuario: async function (req, res) {
+        try {
+            var usuarioId = req.params.id;
+            var usuario = await Usuario.findById(usuarioId);
+
+            if (!usuario) {
+                return res.status(404).send({ message: 'No se encontro el usuario' });
+            }
+            return res.status(200).send({ usuario });
+        } catch (err) {
+            return res.status(500).send({ message: 'Error al obtener los datos' });
+        }
+    }
+
+}
+
+module.exports = userController;
--- routes/rutas.js
+++ routes/rutas.js
@@ -1,6 +1,7 @@
 'use strict'
 var express= require('express');
 var ConcesionarioController= require('../controllers/dandoAlas');
+const userController = require('../controllers/usuario.controller');
 var router=express.Router();
 
 
@@ -8,5 +9,14 @@ var router=express.Router();
 router.get('/home',ConcesionarioController.home); //para poder conectarme por primera vez
 //guardar informacion del vuelo
 router.post('/guardar-vuelo',ConcesionarioController.saveVuelo);
+
+//guardar informacion del usuario
+router.post('/guardar-usuario',userController.saveUsuario);
+
+//obtener informacion de los usuarios
+router.get('/obtener-usuarios',userController.getUsuarios);
+
+//obtener informacion del usuario
+router.get('/obtener-usuario/:id?',userController.getUsuario);
 
 module.exports = router;
