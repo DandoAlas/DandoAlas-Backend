7bbdf13b8be77eae8beef779fcc31e5e5db6c931
Ricardo Terán <39249155+Rikardo816@users.noreply.github.com>
2023-09-10 16:14:40 -0500

    feat(PAYPAL): YA ESTA

---
+++ controllers/payment.controller.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const PAYPAL_API = 'https://api-m.sandbox.paypal.com';
+const PAYPAL_API_CLIENT = 'AQpHDVdKE3lryT5PDUkrhFvyaGPSxBk4yxaNqo9nmaYon-AXFaORqo8-bqtWANJg_3ToMPFhkU-4rDIu';
+const PAYPAL_API_SECRET = 'EEMecI2eDx6K1qWcJi0zgAmcOYf9kiz40WQzgRAdDFrQU-toiIMz1L9pWxrXl_onLlJkaf3omq7nDdGx';
+
+const paymentController = {
+    createOrder: async (req, res) => {
+        const order = {
+            intent: 'CAPTURE',
+            purchase_units: [
+                {
+                    amount: {
+                        currency_code: 'USD',
+                        value: '100.00'
+                    }
+                },
+            ],
+            application_context: {
+                brand_name: `Dando Alas`,
+                landing_page: 'NO_PREFERENCE',
+                user_action: 'PAY_NOW',
+                return_url: 'http://localhost:3600/capture-order',
+                cancel_url: 'http://localhost:3600/cancel-order'
+            }
+
+        }
+        const params = new URLSearchParams();
+        params.append('grant_type', 'client_credentials');
+        const { data: { access_token } } = await axios.post(`${PAYPAL_API}/v1/oauth2/token`, params, {
+            auth: {
+                username: PAYPAL_API_CLIENT,
+                password: PAYPAL_API_SECRET
+            }
+        })
+
+        const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders`, order, {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+        })
+        return res.json(response.data);
+    },
+
+    captureOrder: async (req, res) => {
+        const { token } = req.query;
+        
+        const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders/${token}/capture`, {}, {
+            auth: {
+                username:  PAYPAL_API_CLIENT,
+                password: PAYPAL_API_SECRET
+            }
+        })
+        return res.send('payed');
+    },
+    cancelOrder: async (req, res) => {
+        res.json('cancel order');
+    }
+}
+
+module.exports = paymentController;
--- routes/rutas.js
+++ routes/rutas.js
@@ -6,6 +6,7 @@ const userController = require('../controllers/usuario.controller');
 const authController = require('../auth/auth.controller');
 const pasajeroController = require('../controllers/pasajeros.controller');
 const pagoController = require('../controllers/pago.controller');
+const paymentController = require('../controllers/payment.controller');
 
 //======== LOGIN =========//
 //registrar usuario
@@ -73,5 +74,10 @@ router.delete('/eliminar-pasajero/:id?',pasajeroController.deletePasajero);
 
 
 router.post('/guardar-pago', pagoController.savePago);
+
+//PAYPAL
+router.get('/create-order', paymentController.createOrder);
+router.get('/capture-order', paymentController.captureOrder);
+router.get('/cancel-order', paymentController.cancelOrder);
 
 module.exports = router;
